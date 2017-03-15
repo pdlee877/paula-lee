f42b8d714f9d6763c3a8e809e1f947ced7cdd7ca
Paula Lee <pdlee877@gmail.com>
2017-03-15 14:07:44 -0700

    add waypoint in jQuery for sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
-
     
-})
+    $('.js--section-about-me').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
+    });
+    
+    
+    
+    
+});
