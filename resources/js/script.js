53b9a4a78d2776e52e7a630c4c252501511b1277
Paula Lee <pdlee877@gmail.com>
2017-03-15 14:38:18 -0700

    add navigation scroll

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     
+    
+    /* Stick navigation */
     $('.js--section-about-me').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -11,6 +13,22 @@ $(document).ready(function() {
     });
     
     
-    
+    /* Navigation scroll */
+    $(function() {
+       $('a[href*=#]:not([href=#])').click(function() {
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+              if (target.length) {
+                  $('html, body').animate({
+                      scrollTop: target.offset().top
+                  }, 1000);
+                  return false;
+              }
+          }
+           
+       });
+    });
+  
     
 });
